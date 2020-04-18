914a6eed014e80a4ee8bfe0011205dc59c85c217
alex susanu <alexsusanu@icloud.com>
2020-04-18 12:07:07 +0100

    database functions file

--- database_func.py
+++ database_func.py
@@ -1,10 +1,10 @@
 from definitions import CONNECTION, CURSOR
+from definitions import CREATE_EMPLOYEES, INSERT_EMPLOYEES
 
 def read_sql_script(sql_file, *args)
     f = open('sql_scripts/create.sql', 'r')
     sql_file = f.read()
     f.close()
     CURSOR.execute(sql_file, *args)
-
 
 
